ce8a426472314fd31c39ba5e0f8b3fef3f383929
Wendy-Xiao <wendyxiao0609@gmail.com>
2022-03-10 15:35:48 -0800

    add length limit in bash file

--- run_bash/test_primer.sh
+++ run_bash/test_primer.sh
@@ -4,14 +4,19 @@ cd ../script
 
 # Multi-news dataset
 DATA_NAME="multi_news"
+LENGTH_LIMIT=256
 # Multi-xscience dataset
 DATA_NAME="multi_x_science_sum"
+LENGTH_LIMIT=128
 # Wikisum dataset
 DATA_NAME="wikisum"
+LENGTH_LIMIT=128
 # wcep dataset
 DATA_NAME="wcep"
+LENGTH_LIMIT=50
 # arxiv dataset
 DATA_NAME="arxiv"
+LENGTH_LIMIT=300
 
 
 # original PRIMER model
@@ -26,5 +31,6 @@ nohup python primer_main.py  \
                 --primer_path ../${MODEL_PATH} \
                 --num_workers 0 \
                 --progress_bar_refresh_rate 50 \
+                --max_length_tgt ${LENGTH_LIMIT} \
         > ../test_${DATA_NAME}_${MODEL_NAME}.out &
 
